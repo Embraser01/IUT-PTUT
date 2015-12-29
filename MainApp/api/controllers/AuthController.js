6d136704c7a3c1f218e772fab1a6c6b872a34eb3
Embraser01 <embraser01@gmail.com>
2015-12-29 01:53:40 +0100

    Fix Auth Controller

--- MainApp/api/controllers/AuthController.js
+++ MainApp/api/controllers/AuthController.js
@@ -1,13 +1,61 @@
 module.exports = {
+
+    //===== VIEWS =====//
+
     login: function (req, res) {
+        if (req.session['login_error']) {
+            var error = "Une erreur est survenu";
+
+            switch (error) {
+                case 1:
+                    error = "Mauvaise combinaison email/ mot de passe";
+                    break;
+            }
+            req.session['login_error'] = null;
+            return res.view('auth/login', {error: error});
+        }
+
         return res.view('auth/login');
     },
 
     signup: function (req, res) {
+
+        if (req.session['signup_error']) {
+            var error = "Une erreur est survenu";
+
+            switch (error) {
+            }
+
+            req.session['signup_error'] = null;
+            return res.view('auth/signup', {error: error});
+        }
+
         return res.view('auth/signup');
     },
 
 
+    //===== PROCESS =====//
+
+    processSignup: function (req, res) {
+
+        User.signup({
+            name: req.param('name'),
+            firstname: req.param('firstname'),
+            mail: req.param('mail'),
+            password: req.param('password'),
+            passwordConfirmation: req.param('passwordConfirmation')
+        }, function (err, user) {
+            if (err){
+                req.session['signup_error'] = 1;
+                return res.redirect('/auth/signup');
+            }
+
+            req.session.user = user;
+
+            if (req.wantsJSON) return res.ok('Signup successful !');
+            return res.redirect('/');
+        });
+    },
 
     processLogin: function (req, res) {
 
@@ -15,14 +63,14 @@ module.exports = {
             mail: req.param('mail'),
             password: req.param('password')
         }, function (err, user) {
-            if (err) return res.ok(err);
+            if (err){
+                req.session['login_error'] = 1;
+                return res.redirect('/auth/login');
+            }
 
             req.session.user = user;
 
-            if (req.wantsJSON) {
-                return res.ok('Login successful !');
-            }
-
+            if (req.wantsJSON) return res.ok('Login successful !');
             return res.redirect('/');
         });
     },
@@ -34,49 +82,6 @@ module.exports = {
             return res.ok('Logged out successfully!');
         }
 
-        User.subscribe(req, user, 'message');
-
-        // Get updates about users being created
-        User.watch(req);
-
-        // Get updates about rooms being created
-        MindMap.watch(req);
-
-        // Publish this user creation event to every socket watching the User model via User.watch()
-        User.publishCreate(user, req);
-
         return res.redirect('/auth/login');
-    },
-
-    processSignup: function (req, res) {
-
-        User.signup({
-            name: req.param('name'),
-            firstname: req.param('firstname'),
-            mail: req.param('mail'),
-            password: req.param('password'),
-            passwordConfirmation: req.param('passwordConfirmation')
-        }, function (err, user) {
-            if (err) return res.ok(err);
-
-            req.session.user = user;
-
-            if (req.wantsJSON) {
-                return res.ok('Signup successful !');
-            }
-
-            User.subscribe(req, user, 'message');
-
-            // Get updates about users being created
-            User.watch(req);
-
-            // Get updates about rooms being created
-            Room.watch(req);
-
-            // Publish this user creation event to every socket watching the User model via User.watch()
-            User.publishCreate(user, req);
-
-            return res.redirect('/');
-        });
     }
 };
