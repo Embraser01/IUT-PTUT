35c517168f2d7c3d8222c2b51c986d5fd4eb4889
Embraser01 <embraser01@gmail.com>
2016-01-03 18:22:23 +0100

    Add an escape service and remake update action (for multiple edition)

--- MainApp/api/controllers/NodeController.js
+++ MainApp/api/controllers/NodeController.js
@@ -1,3 +1,42 @@
+function queryNodesUpdate(nodes) {
+    var request_node = "UPDATE Node SET ";
+    var part_req1 = "parent_node = CASE id ";
+    var part_req2 = "label = CASE id";
+    var part_req3 = "WHERE id IN (";
+
+    _.forEach(nodes, function (n) {
+        part_req1 += "WHEN " + n.id + " THEN " + n.parent_node + " ";
+        part_req2 += "WHEN " + n.id + " THEN " + n.label + " ";
+        part_req3 += n.id + ",";
+    });
+    part_req1 += "ELSE parent_node END, ";
+    part_req2 += "ELSE label END ";
+    part_req3 = part_req3.slice(0, -1) + ");";
+
+    request_node += part_req1 + part_req2 + part_req3;
+    return EscapeService.escape(request_node);
+}
+
+function queryStylesUpdate(nodes) {
+    var request_style = "UPDATE Style SET ";
+    var part_req1 = "style = CASE node ";
+    var part_req2 = "fold = CASE node";
+    var part_req3 = "WHERE owner = " + req.user.id + " AND node IN (";
+
+    _.forEach(nodes, function (n) {
+        part_req1 += "WHEN " + n.id + " THEN " + SerializeService.serialize(n.style) + " ";
+        part_req2 += "WHEN " + n.id + " THEN " + n.fold + " ";
+        part_req3 += n.id + ",";
+    });
+    part_req1 += "ELSE style END, ";
+    part_req2 += "ELSE fold END ";
+    part_req3 = part_req3.slice(0, -1) + ");";
+
+    request_style += part_req1 + part_req2 + part_req3;
+    return EscapeService.escape(request_style);
+}
+
+
 module.exports = {
 
     new: function (req, res) {
@@ -51,60 +90,58 @@ module.exports = {
         var updateStyle = req.param('style');
 
 
-        var formatted_nodes = [];
+        var ids = [];
         _.forEach(nodes, function (n) {
-            formatted_nodes.push({
-                label: n.label,
-                parent_node: n.parent_node
-            });
-        });
-
-        var formatted_nodes_id = [];
-        _.forEach(nodes, function (n) {
-            formatted_nodes_id.push(n.id);
+            ids.push(n.id);
         });
 
 
-        Node.update({where: {id: formatted_nodes_id}}, formatted_nodes).exec(function (err, new_nodes) {
-            if (err) console.log(err);
+        Node.query(queryNodesUpdate(nodes), function (err) {
+            if (err) return console.log(err);
 
             if (updateStyle === 'yes') {
+                Style.query(queryStylesUpdate(nodes), function (err) {
+                    if (err) return console.log(err);
 
-                var formatted_styles = [];
-                _.forEach(nodes, function (n) {
-                    formatted_styles.push({
-                        style: n.style,
-                        fold: n.fold
+                    Node.find({where: {id: ids}}).populate('styles').exec(function (err, nodes) {
+                        if (err) return console.log(err);
+
+                        if (nodes) {
+                            _.forEach(nodes, function (n) {
+                                // On laisse un seul style
+                                n.style = SerializeService.unserialize(n.styles[0].style);
+                                n.styles = null;
+
+                                // Remplace 0 par null pour le parent
+                                if (n.parent_node === 0) n.parent_node = null;
+                            });
+                        }
+
+                        MindMapMsgService.send('Update_nodes_w_style', req, nodes); // Notify users
+                        return res.json(nodes);
                     });
                 });
 
-                var formatted_styles_id = [];
-                _.forEach(nodes, function (n) {
-                    formatted_nodes_id.push({
-                        node: n.id,
-                        owner: req.session.user.id
-                    });
-                });
+            } else {
+                Node.find({where: {id: ids}}).populate('styles').exec(function (err, nodes) {
+                    if (err) return console.log(err);
 
-                Style.update({where: formatted_styles_id}, formatted_styles).exec(function (err, new_styles) {
-                    if (err) return;
+                    if (nodes) {
+                        _.forEach(nodes, function (n) {
+                            // On laisse un seul style
+                            n.style = SerializeService.unserialize(n.styles[0].style);
+                            n.styles = null;
 
-                    if (new_nodes) {
-
-                        _.forEach(new_nodes, function (n, key) {
-                            n.style = SerializeService.unserialize(new_styles[key].style);
-                            //n.parent_node = n.parent_node.id;
-
+                            // Remplace 0 par null pour le parent
                             if (n.parent_node === 0) n.parent_node = null;
                         });
                     }
-                    MindMapMsgService.send('Update_nodes_w_style', req, new_nodes); // Notify users
-                });
-            } else {
-                MindMapMsgService.send('Update_nodes', req, new_nodes); // Notify users
-            }
 
-            return res.json(new_nodes);
+                    MindMapMsgService.send('Update_nodes', req, nodes); // Notify users
+                    return res.json(nodes);
+
+                });
+            }
         });
     },
 
