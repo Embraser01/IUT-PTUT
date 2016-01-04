dda40b93996bdf3fc0b48aeb7b89d399575277ea
Embraser01 <embraser01@gmail.com>
2016-01-04 21:15:40 +0100

    Clean auth policies

--- api/policies/isAuthenticated.js
+++ api/policies/isAuthenticated.js
@@ -2,42 +2,25 @@ var passport = require('passport');
 
 module.exports = function (req, res, next) {
 
-    if(req.isSocket)
-    {
-        if(req.session &&
-            req.session.passport &&
-            req.session.passport.user)
-        {
-            //Use this:
+    if (req.isSocket) {
+        if (req.session && req.session.passport && req.session.passport.user) {
 
             // Initialize Passport
             passport.initialize()(req, res, function () {
                 // Use the built-in sessions
                 passport.session()(req, res, function () {
-                    // Make the user available throughout the frontend
-                    res.locals.user = req.user;
-                    //the user should be deserialized by passport now;
                     next();
                 });
             });
-
-            //Or this if you dont care about deserializing the user:
-            //req.user = req.session.passport.user;
-            //return next();
-
         }
-        else{
+        else {
             res.json(401);
         }
-
-
     }
     else if (req.isAuthenticated()) {
         return next();
     }
-    else{
-        // User is not allowed
-        // (default res.forbidden() behavior can be overridden in `config/403.js`)
+    else {
         return res.redirect('/auth/login');
     }
 };
--- api/policies/isNotAuthenticated.js
+++ api/policies/isNotAuthenticated.js
@@ -1,17 +1,14 @@
+var passport = require('passport');
+
 module.exports = function (req, res, next) {
 
     if (req.isSocket) {
-        if (req.session &&
-            req.session.passport && !req.session.passport.user) {
-            //Use this:
+        if (req.session && req.session.passport && !req.session.passport.user) {
 
             // Initialize Passport
             passport.initialize()(req, res, function () {
                 // Use the built-in sessions
                 passport.session()(req, res, function () {
-                    // Make the user available throughout the frontend
-                    res.locals.user = req.user;
-                    //the user should be deserialized by passport now;
                     next();
                 });
             });
@@ -24,8 +21,6 @@ module.exports = function (req, res, next) {
         return next();
     }
     else {
-        // User is not allowed
-        // (default res.forbidden() behavior can be overridden in `config/403.js`)
         return res.redirect('/');
     }
 };
