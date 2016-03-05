eb512a7a7a55870bc788cdf82d03adb1c1e4c504
Kocal <kocal@Konata.lan>
2016-03-05 20:35:49 +0100

    Whitespace!

--- config/models.js
+++ config/models.js
@@ -24,7 +24,7 @@ module.exports.models = {
   * How and whether Sails will attempt to automatically rebuild the          *
   * tables/collections/etc. in your schema.                                  *
   *                                                                          *
-  * See http://sailsjs.org/#!/documentation/concepts/ORM/model-settings.html  *
+  * See http://sailsjs.org/#!/documentation/concepts/ORM/model-settings.html *
   *                                                                          *
   ***************************************************************************/
   migrate: 'safe',
