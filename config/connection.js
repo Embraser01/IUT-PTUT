cacc6a591e6d5570252c623148f06d06321fb116
ALLIAUME Hugo <kocal@live.fr>
2016-03-10 15:02:30 +0100

    Je reprend le travail
    dsl lé mek

---
+++ config/connection.js
@@ -0,0 +1,9 @@
+module.exports.connections = {
+    mysql: {
+        adapter: 'sails-mysql',
+        host: 'localhost',
+        user: 'mindmap',
+        password: 'mindmap',
+        database: 'mindmap'
+    }
+};
